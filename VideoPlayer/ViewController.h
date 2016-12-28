c268dad89220857f401caa355ded41a7fbccc740
buoumall <buoumall@buoumalldeimac2.lan>
2016-12-28 13:42:48 +0800

    Initial Commit

---
+++ VideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayer
+//
+//  Created by buoumall on 2016/12/28.
+//  Copyright © 2016年 buoumall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
